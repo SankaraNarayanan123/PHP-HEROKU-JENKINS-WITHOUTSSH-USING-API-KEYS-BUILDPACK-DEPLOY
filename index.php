5d7cc7b67381f6865cb94b6599d0b7ac442f2f1b
SankaraNarayanan123 <38694997+SankaraNarayanan123@users.noreply.github.com>
2022-11-06 00:41:48 +0530

    Delete index.php

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-   echo "Hello World PHP Application Deployed to Heroku Using Jenkins by SankaraNarayanan through API Keys without SSH using GitHub PHP BuildPack but not WAMP Stack Bitnami Application!";
-?>
